4519c1707bfc654cd4fdd77ee58e6e90de9a9eb7
tuckerbrown5 <tucker.brown@students.fhu.edu>
2024-01-31 14:40:45 -0600

    Math functions/Pandoras Box function/ greeting function

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const facts = require('./facts.json')
 
+
 const express = require('express')
 const app = express()
 
@@ -17,38 +18,63 @@ app.get("/", (req, res) => {
 
 })
 
-// http://localhost:3000/greet?name=kaylee&dob=2002
-app.get('/greet', (req, res)=> {
-    console.log(req.query)
 
-    res.send(`hey, ${req.query.name}`)
-})
+// Greeting Function
+// http://localhost:3000/greet?name=kaylee&year=2002
+app.get('/greet', (req, res) => {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - req.query.year;
+    res.send(`Hello, ${req.query.name}! You are ${age} or ${age - 1} years old.`);
+});
 
-app.get('/math/:num1/:op/:num2', (req, res)=> {
-    console.log( req.params )
-    res.send(`${req.params.num1}`)
-})
+//  Math Operations 
+// http://localhost:3000/math/5/add/3
+app.get('/math/:num1/:op/:num2', (req, res) => {
+    const num1 = parseInt(req.params.num1);
+    const num2 = parseInt(req.params.num2);
+    let result;
 
-app.get('/pandorasbox', (req, res)=> {
+    switch(req.params.op) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num1 / num2;
+            break;
+        default:
+            result = 'Invalid operation';
+    }
 
-    // do the work
+    res.send(`${result}`);
+});
 
-    fetch("https://icanhazdadjoke.com/", { 
-        headers: {
-            "Accept": "application/json"
-        }
+// Pandora's Box Function (Random Dad Joke or Random Fact)
+// http://localhost:3000/pandorasbox (then reload the page to see the random fact or dad joke)
+
+app.get('/pandorasbox', (req, res) => {
+    const choice = Math.floor(Math.random() * 2); // Randomly generates 0 or 1
+
+    if (choice === 0) {
+        // Fetch a dad joke
+        fetch("https://icanhazdadjoke.com/", { 
+            headers: {
+                "Accept": "application/json"
+            }
         })
-        .then( res => res.json() )
-        .then( (data) => {
-            console.log(data)
-            res.render('pandorasbox', {title: "Pandora's Box", message: data.joke} )
-        })
-
-    //const message = "DAD JOKE"
-    // const length = facts.length;
-    // const random =  Math.floor( Math.random() * length)
-    // const fact4 = facts[random].fact
-
-    // res.render('pandorasbox', {title: "Pandora's Box", message:fact4} )
-
-})
+        .then(response => response.json())
+        .then(data => {
+            res.render('pandorasbox', { title: "Pandora's Box", message: data.joke });
+        });
+    } else {
+        // Pick a random fact from facts.json
+        const randomIndex = Math.floor(Math.random() * facts.length);
+        const fact = facts[randomIndex].fact;
+        res.render('pandorasbox', { title: "Pandora's Box", message: fact });
+    }
+});
